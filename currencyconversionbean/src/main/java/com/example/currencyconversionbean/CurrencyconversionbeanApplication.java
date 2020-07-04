d7cc2c336fed417e9a2767ab9f700bb23e3e984b
Demipo <41957499+Demipo@users.noreply.github.com>
2020-07-04 23:59:09 +0100

    initial commit

---
+++ currencyconversionbean/src/main/java/com/example/currencyconversionbean/CurrencyconversionbeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyconversionbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconversionbeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyconversionbeanApplication.class, args);
+    }
+
+}
---
+++ currencyconversionbean/src/test/java/com/example/currencyconversionbean/CurrencyconversionbeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyconversionbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconversionbeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
