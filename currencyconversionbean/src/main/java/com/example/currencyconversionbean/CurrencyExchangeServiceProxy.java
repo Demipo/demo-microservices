725407b50f622f425c860cf900dbd9a15e010087
Demipo <41957499+Demipo@users.noreply.github.com>
2020-07-14 20:41:58 +0100

    enable eureka naming server

--- currencyconversionbean/src/main/java/com/example/currencyconversionbean/CurrencyExchangeServiceProxy.java
+++ currencyconversionbean/src/main/java/com/example/currencyconversionbean/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.example.currencyconversionbean;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 @FeignClient(name="forex-service" , url="localhost:8000")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue
